26abf36829bc5494a6c13132a8a6cd4396967147
David Palomino <luisdavidpa96@gmail.com>
2020-10-01 00:15:02 -0500

    01/10/2020 V. Final

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,5 +1,30 @@
 // Check Off Specific Todos By Clicking
-$("li").click(function () {
+$("ul").on("click", "li", function () {
   // if li is gray change to gray turn it black else gray
   $(this).toggleClass("completed");
+});
+
+// Click on X to delete To-do
+$("ul").on("click", "span", function (event) {
+  $(this)
+    .parent()
+    .fadeOut(500, function () {
+      $(this).remove();
+    });
+  event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function (event) {
+  if (event.which === 13) {
+    //   grabbing new todo text from enter
+    let todoText = $(this).val();
+    $(this).val("");
+    $("ul").append(
+      "<li><span><i class='far fa-trash-alt'></i></span> " + todoText + "</li>"
+    );
+  }
+});
+
+$(".fa-plus").click(function () {
+  $("input[type='text']").fadeToggle();
 });
